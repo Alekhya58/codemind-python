7d46b0861c1aedacde14791a7ef73becab20d360
Alekhya58 <97424622+Alekhya58@users.noreply.github.com>
2022-05-15 17:23:11 +0530

    n=int(input()) temp=n s=0 while n!=0:     d=n%10     n=n//10     p=1     for i in range(1,d+1):         p=p*i     s=s+p if(s==temp):     print('The number',s,'is a strong number') else:     print('The number',temp,'is not a strong number')

---
+++ Strong_Number.py
@@ -0,0 +1,14 @@
+n=int(input())
+temp=n
+s=0
+while n!=0:
+    d=n%10
+    n=n//10
+    p=1
+    for i in range(1,d+1):
+        p=p*i
+    s=s+p
+if(s==temp):
+    print('The number',s,'is a strong number')
+else:
+    print('The number',temp,'is not a strong number')
